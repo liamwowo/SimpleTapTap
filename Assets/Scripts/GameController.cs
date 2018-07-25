3d07aa481c9bb14e702d62c88f357ec2f7a2db36
Liam <szwowak@gmail.com>
2018-07-25 19:49:10 +0200

    - assets for TapTap game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    [SerializeField]
+    UIController _uiController;
+    [SerializeField]
+    int _countdownSec=3;
+    [SerializeField]
+    int _gameTimeSec = 10;
+
+    int score = 0;
+    int highScore = 0;
+
+    private void Start() {
+        if (_uiController) {
+            _uiController.ShowMenuCanvas();
+            _uiController.UpdateHighScore(highScore);
+        }
+    }
+
+    public void NewGame() {
+        score = 0;
+        if (_uiController) {
+            _uiController.UpdateInGameScore(score);
+            _uiController.CountdownToStart(_countdownSec);
+        }
+        StartCoroutine(GameTimeCoroutine());
+    }
+
+    public void OnTap() {
+        score++;
+        if (_uiController)
+            _uiController.UpdateInGameScore(score);
+    }
+
+    void GameOver() {
+        if (score > highScore)
+            highScore = score;
+        if (_uiController) {
+            _uiController.UpdateGameOverScore(score);
+            _uiController.UpdateHighScore(highScore);
+            _uiController.ShowGameOverCanvas();
+        }
+    }
+
+    IEnumerator GameTimeCoroutine() {
+        yield return new WaitForSeconds(_countdownSec);
+        WaitForEndOfFrame wait = new WaitForEndOfFrame();
+        float timeLeft = _gameTimeSec;
+        while (timeLeft > 0) {
+            if (_uiController)
+                _uiController.UpdateGameTimeSlider(timeLeft / _gameTimeSec);
+            yield return wait;
+            timeLeft -= Time.deltaTime;
+        }
+        GameOver();
+    }
+
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+    [SerializeField]
+    Transform _menuCanvas;
+    [SerializeField]
+    Transform _gameCanvas;
+    [SerializeField]
+    Transform _gameOverCanvas;
+    [SerializeField]
+    Transform _countdownCanvas;
+    [SerializeField]
+    Text _countdownText;
+    [SerializeField]
+    Text _inGamesScore;
+    [SerializeField]
+    Text _menuHighScore;
+    [SerializeField]
+    Slider _gameTimeSlider;
+    [SerializeField]
+    Text _gameOverScore;
+    [SerializeField]
+    Text _gameOverHighScore;
+
+    public void ShowMenuCanvas() {
+        _menuCanvas.gameObject.SetActive(true);
+        _gameCanvas.gameObject.SetActive(false);
+        _gameOverCanvas.gameObject.SetActive(false);
+        _countdownCanvas.gameObject.SetActive(false);
+    }
+
+    public void ShowGameOverCanvas() {
+        _gameOverCanvas.gameObject.SetActive(true);
+        _menuCanvas.gameObject.SetActive(false);
+        _gameCanvas.gameObject.SetActive(false);
+    }
+
+    public void CountdownToStart(int seconds) {
+        _menuCanvas.gameObject.SetActive(false);
+        _gameOverCanvas.gameObject.SetActive(false);
+        _countdownCanvas.gameObject.SetActive(true);
+        StartCoroutine(CountdownCoroutine(seconds));
+    }
+
+    public void UpdateInGameScore(int score) {
+        _inGamesScore.text = score.ToString();
+    }
+
+    public void UpdateGameOverScore(int score) {
+        _gameOverScore.text = score.ToString();
+    }
+
+    public void UpdateHighScore(int highScore) {
+        _gameOverHighScore.text = _menuHighScore.text = highScore.ToString();
+    }
+
+    public void UpdateGameTimeSlider(float percentage) {
+        _gameTimeSlider.value = percentage;
+    }
+
+    IEnumerator CountdownCoroutine(int seconds) {
+        WaitForSeconds wait = new WaitForSeconds(1f);
+        int timer = 0;
+        while (timer < seconds) {
+            _countdownText.text = (seconds - timer).ToString();
+            for(int i=0; i<3; i++)
+                yield return AddDotToCountdown();
+            yield return new WaitForSeconds(0.25f);
+            timer++;
+        }
+        _countdownText.text = "GO!";
+        yield return new WaitForSeconds(0.3f);
+        ShowGameCanvas();
+    }
+
+    IEnumerator AddDotToCountdown() {
+        WaitForSeconds waitOneFourth = new WaitForSeconds(0.25f); 
+        yield return waitOneFourth;
+        _countdownText.text += ".";
+    }
+
+    void ShowGameCanvas() {
+        _gameCanvas.gameObject.SetActive(true);
+        _countdownCanvas.gameObject.SetActive(false);
+    }
+}
